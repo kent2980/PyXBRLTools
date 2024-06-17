48e2c20bcfa5a613969260d236610f1aa6074113
kent2980 <kent2980@gmail.com>
2024-06-18 01:14:02 +0900

    feat: Add link label management functions

--- PyXBRLTools/tests/xbrl_manager/test_label_manager.py
+++ PyXBRLTools/tests/xbrl_manager/test_label_manager.py
@@ -1,7 +1,6 @@
 import pytest
 from pandas import DataFrame
 from PyXBRLTools.xbrl_manager.label_manager import LabelManager
-from PyXBRLTools.xbrl_parser.label_parser import LabelParser
 from pathlib import Path
 
 def get_current_dir():
@@ -20,8 +19,8 @@ def label_manager() ->  LabelManager:
     test_dir = get_current_dir() / "data" / "edjp"
     return LabelManager(test_dir.as_posix())
 
-def test_set_label(label_manager):
-    result = label_manager.set_label(get_output_dir())
+def test_set_link_labels(label_manager):
+    result = label_manager.set_link_labels(get_output_dir())
     result.to_json(str(get_current_dir() / "output" / "label.json"))
     assert isinstance(result, LabelManager)
     assert isinstance(result.to_DataFrame(), DataFrame)
@@ -37,21 +36,63 @@ def test_set_label(label_manager):
         }
 
 def test_set_label_with_document_type(label_manager):
-    result = label_manager.set_label(get_output_dir(), document_type="sm")
+    result = label_manager.set_link_labels(get_output_dir(), document_type="sm")
     assert isinstance(result, LabelManager)
     assert isinstance(result.to_DataFrame(), DataFrame)
     assert len(result.to_DataFrame()) > 0
 
 def test_set_label_with_invalid_document_type(label_manager):
-    result = label_manager.set_label(get_output_dir(), document_type="bs")
+    result = label_manager.set_link_labels(get_output_dir(), document_type="bs")
     assert isinstance(result, LabelManager)
     assert isinstance(result.to_DataFrame(), DataFrame)
     assert len(result.to_DataFrame()) == 0
 
-def test_set_label_with_html_files(label_manager):
-    # Add test cases to check if the label is set correctly for HTML files
-    pass
+def test_set_link_locs(label_manager):
+    result = label_manager.set_link_locs(get_output_dir())
+    print(result.to_DataFrame().iloc[0].to_dict())
+    assert isinstance(result, LabelManager)
+    assert isinstance(result.to_DataFrame(), DataFrame)
+    assert len(result.to_DataFrame()) > 0
+    assert result.to_DataFrame().iloc[0].to_dict() == {
+        'xlink_type': 'locator',
+        'xlink_schema': 'tse-acedjpfr-58200-2024-03-31-01-2024-05-14.xsd',
+        'xlink_href': 'tse-acedjpfr-58200_CostForTheMeetingOfShareholdersEL',
+        'xlink_label': 'CostForTheMeetingOfShareholdersEL'
+        }
 
-def test_set_label_with_non_html_files(label_manager):
-    # Add test cases to check if the label is set correctly for non-HTML files
-    pass
+def test_set_link_locs_with_document_type(label_manager):
+    result = label_manager.set_link_locs(get_output_dir(), document_type="sm")
+    assert isinstance(result, LabelManager)
+    assert isinstance(result.to_DataFrame(), DataFrame)
+    assert len(result.to_DataFrame()) > 0
+
+def test_set_link_label_arcs(label_manager):
+    result = label_manager.set_link_label_arcs(get_output_dir())
+    assert isinstance(result, LabelManager)
+    assert isinstance(result.to_DataFrame(), DataFrame)
+    assert len(result.to_DataFrame()) > 0
+    assert result.to_DataFrame().iloc[0].to_dict() == {
+        'xlink_type': 'arc',
+        'xlink_arcrole': 'http://www.xbrl.org/2003/arcrole/concept-label',
+        'xlink_from': 'CostForTheMeetingOfShareholdersEL',
+        'xlink_to': 'label_CostForTheMeetingOfShareholdersEL',
+        'xlink_schema': 'tse-acedjpfr-58200-2024-03-31-01-2024-05-14.xsd'
+                }
+
+def test_set_link_label_arcs_with_document_type(label_manager):
+    result = label_manager.set_link_label_arcs(get_output_dir(), document_type="sm")
+    assert isinstance(result, LabelManager)
+    assert isinstance(result.to_DataFrame(), DataFrame)
+    assert len(result.to_DataFrame()) > 0
+
+def test_set_role_refs(label_manager):
+    result = label_manager.set_role_refs(get_output_dir())
+    assert isinstance(result, LabelManager)
+    assert isinstance(result.to_DataFrame(), DataFrame)
+    assert len(result.to_DataFrame()) > 0
+    assert result.to_DataFrame().iloc[0].to_dict() == {
+        'Role_URI': 'http://disclosure.edinet-fsa.go.jp/jpcrp/std/alt/Consolidated/role/label',
+        'xlink_type': 'simple',
+        'xlink_schema': '../jpcrp_rt_2023-12-01.xsd',
+        'xlink_href': 'rol_std_altConsolidatedLabel'
+        }
