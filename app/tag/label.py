63ec6c0cea7ebc9cf3c138053d68811b8d7eef7c
kent2980 <32239427+kent2980@users.noreply.github.com>
2024-07-08 13:13:08 +0900

    label.py の更新

--- app/tag/label.py
+++ app/tag/label.py
@@ -4,53 +4,50 @@ from typing import Optional
 @dataclass
 class LabelValue:
     """ラベル情報を格納するクラス"""
-    xlink_type: Optional[str] = None
-    xlink_label: Optional[str] = None
-    xlink_role: Optional[str] = None
-    xml_lang: Optional[str] = None
-    id: Optional[str] = None
-    label: Optional[str] = None
-    xlink_schema: Optional[str] = None
+    xlink_type: Optional[str] = field(default=None)
+    xlink_label: Optional[str] = field(default=None)
+    xlink_role: Optional[str] = field(default=None)
+    xml_lang: Optional[str] = field(default=None)
+    id: Optional[str] = field(default=None)
+    label: Optional[str] = field(default=None)
+    xlink_schema: Optional[str] = field(default=None)
 
     @classmethod
     def keys(cls):
         return list(cls().__dict__.keys())
-
 
 @dataclass
 class LabelLoc:
     """loc要素情報を格納するクラス"""
-    xlink_type: Optional[str] = None
-    xlink_label: Optional[str] = None
-    xlink_schema: Optional[str] = None
-    xlink_href: Optional[str] = None
+    xlink_type: Optional[str] = field(default=None)
+    xlink_label: Optional[str] = field(default=None)
+    xlink_schema: Optional[str] = field(default=None)
+    xlink_href: Optional[str] = field(default=None)
 
     @classmethod
     def keys(cls):
         return list(cls().__dict__.keys())
-
 
 @dataclass
 class LabelArc:
     """arc要素情報を格納するクラス"""
-    xlink_type: Optional[str] = None
-    xlink_from: Optional[str] = None
-    xlink_to: Optional[str] = None
-    xlink_arcrole: Optional[str] = None
-    xlink_schema: Optional[str] = None
+    xlink_type: Optional[str] = field(default=None)
+    xlink_from: Optional[str] = field(default=None)
+    xlink_to: Optional[str] = field(default=None)
+    xlink_arcrole: Optional[str] = field(default=None)
+    xlink_schema: Optional[str] = field(default=None)
 
     @classmethod
     def keys(cls):
         return list(cls().__dict__.keys())
 
-
 @dataclass
 class LabelRoleRefs:
     """roleRef要素情報を格納するクラス"""
-    role_uri: Optional[str] = None
-    xlink_type: Optional[str] = None
-    xlink_schema: Optional[str] = None
-    xlink_href: Optional[str] = None
+    role_uri: Optional[str] = field(default=None)
+    xlink_type: Optional[str] = field(default=None)
+    xlink_schema: Optional[str] = field(default=None)
+    xlink_href: Optional[str] = field(default=None)
 
     @classmethod
     def keys(cls):
