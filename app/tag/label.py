1d522aff164d7d3cba12b7eb9c5b20f776b78ca3
kent2980 <32239427+kent2980@users.noreply.github.com>
2024-07-08 13:06:53 +0900

    label.py の更新

--- app/tag/label.py
+++ app/tag/label.py
@@ -1,98 +1,57 @@
-from .base import BaseTag
+from dataclasses import dataclass, field
+from typing import Optional
+
+@dataclass
+class LabelValue:
+    """ラベル情報を格納するクラス"""
+    xlink_type: Optional[str] = None
+    xlink_label: Optional[str] = None
+    xlink_role: Optional[str] = None
+    xml_lang: Optional[str] = None
+    id: Optional[str] = None
+    label: Optional[str] = None
+    xlink_schema: Optional[str] = None
+
+    @classmethod
+    def keys(cls):
+        return list(cls().__dict__.keys())
 
 
-class LabelValue(BaseTag):
-	"""ラベル情報を格納するクラス"""
-
-	def __init__(
-		self,
-		xlink_type,
-		xlink_label,
-		xlink_role=None,
-		xml_lang=None,
-		_id=None,
-		label=None,
-		xlink_schema=None,
-	):
-		self.xlink_type = xlink_type
-		self.xlink_label = xlink_label
-		self.xlink_role = xlink_role
-		self.xml_lang = xml_lang
-		self.id = _id
-		self.label = label
-		self.xlink_schema = xlink_schema
-
-	@classmethod
-	def keys(cls):
-		instance = cls(
-			xlink_type=None,
-			xlink_label=None,
-			xlink_role=None,
-			xml_lang=None,
-			_id=None,
-			label=None,
-			xlink_schema=None,
-		)
-		return list(instance.__dict__)
-
-
+@dataclass
 class LabelLoc:
-	"""loc要素情報を格納するクラス"""
+    """loc要素情報を格納するクラス"""
+    xlink_type: Optional[str] = None
+    xlink_label: Optional[str] = None
+    xlink_schema: Optional[str] = None
+    xlink_href: Optional[str] = None
 
-	def __init__(self, xlink_type, xlink_label, xlink_schema=None, xlink_href=None):
-		self.xlink_type = xlink_type
-		self.xlink_label = xlink_label
-		self.xlink_schema = xlink_schema
-		self.xlink_href = xlink_href
-
-	@classmethod
-	def keys(cls):
-		instance = cls(
-			xlink_type=None,
-			xlink_label=None,
-			xlink_schema=None,
-			xlink_href=None,
-		)
-		return list(instance.__dict__)
+    @classmethod
+    def keys(cls):
+        return list(cls().__dict__.keys())
 
 
+@dataclass
 class LabelArc:
-	"""arc要素情報を格納するクラス"""
+    """arc要素情報を格納するクラス"""
+    xlink_type: Optional[str] = None
+    xlink_from: Optional[str] = None
+    xlink_to: Optional[str] = None
+    xlink_arcrole: Optional[str] = None
+    xlink_schema: Optional[str] = None
 
-	def __init__(self, xlink_type, xlink_from, xlink_to, xlink_arcrole, xlink_schema):
-		self.xlink_type = xlink_type
-		self.xlink_from = xlink_from
-		self.xlink_to = xlink_to
-		self.xlink_arcrole = xlink_arcrole
-		self.xlink_schema = xlink_schema
+    @classmethod
+    def keys(cls):
+        return list(cls().__dict__.keys())
 
-	@classmethod
-	def keys(cls):
-		instance = cls(
-			xlink_type=None,
-			xlink_from=None,
-			xlink_to=None,
-			xlink_arcrole=None,
-			xlink_schema=None,
-		)
-		return list(instance.__dict__)
 
+@dataclass
 class LabelRoleRefs:
-	"""roleRef要素情報を格納するクラス"""
+    """roleRef要素情報を格納するクラス"""
+    role_uri: Optional[str] = None
+    xlink_type: Optional[str] = None
+    xlink_schema: Optional[str] = None
+    xlink_href: Optional[str] = None
 
-	def __init__(self, role_uri, xlink_type, xlink_schema, xlink_href):
-		self.role_uri = role_uri
-		self.xlink_type = xlink_type
-		self.xlink_schema = xlink_schema
-		self.xlink_href = xlink_href
-
-	@classmethod
-	def keys(cls):
-		instance = cls(
-			role_uri=None,
-			xlink_type=None,
-			xlink_schema=None,
-			xlink_href=None,
-		)
-		return list(instance.__dict__)
-
+    @classmethod
+    def keys(cls):
+        return list(cls().__dict__.keys())
